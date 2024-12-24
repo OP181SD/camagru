cd79afc0fec18d6437621314b2feac2d86aff670
OP181SD <thejesuisla78@gmail.com>
2024-12-24 13:48:46 +0100

    fixed the volume shared

---
+++ src/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "db"; 
+$database = "mydatabase";
+$username = "myuser";
+$password = "mypassword";
+
+var_dump($host);
+var_dump($database);
+var_dump($username);
+var_dump($password);
+
+?>
