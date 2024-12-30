20fcb0b3aee2eef00e403efa00d482473219973d
OP181SD <thejesuisla78@gmail.com>
2024-12-30 14:42:50 +0100

    change makefile for postgre

--- src/backend/index.php
+++ src/backend/index.php
@@ -1,19 +1,52 @@
 <?php
 
-
 class Database
 {
+    // Static variable for Singleton pattern
+    private static $instance = null;
+
+    // Database connection parameters
+    private $host;
+    private $dbname;
+    private $user;
+    private $password;
+    private $dsn;
+
+    private $pdo;
+
     
+    private function __construct() {
+        $this->host = "db";           
+        $this->dbname = "camagru";    
+        $this->user = "user";         
+        $this->password = "password";
+        $this->dsn = "pgsql:host={$this->host};dbname={$this->dbname}"; 
+
+        try {
+            // Establish connection using PDO
+            $this->pdo = new PDO($this->dsn, $this->user, $this->password);
+            echo "Connexion réussie";
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (Throwable $th) {
+           
+            echo "Error: " . $th->getMessage();
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
 }
 
-
-
-
-
-
-
-
-
-
+// Test the singleton
+$db = Database::getInstance();
 
 ?>
